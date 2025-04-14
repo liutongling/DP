fca3db633664f14b07053617d19548b1887f1747
liutongling <liutl9881@163.com>
2025-04-14 23:21:45 +0800

    完成今日题目， 1534. 统计好三元组

--- Dp.py
+++ Dp.py
@@ -183,8 +183,19 @@ class Solution(object):
             n = -n
         return self.dfsPow(x,n)
 
+    def countGoodTriplets(self, arr: list, a: int, b: int, c: int) -> int:
+        n = len(arr)
+        count = 0
+        for i in range(n-2):
+            for j in range(i+1,n-1):
+                for k in range(j+1,n):
+                    if abs(arr[i]-arr[j])<=a and abs(arr[j]-arr[k])<=b and abs(arr[i]-arr[k])<=c:
+                        print([arr[i],arr[j],arr[k]])
+                        count += 1
+        return count
+
 # leetcode submit region end(Prohibit modification and deletion)
 if __name__ == '__main__':
     s = Solution()
-    val = s.myPow(2,-200000000)
+    val = s.countGoodTriplets([3,0,1,1,9,7],7,2,3)
     print(val)
