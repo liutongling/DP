a3c578fe28c32bd40f4109744793b8a7248b1c81
PCtongling <2594924691@qq.com>
2025-06-10 00:17:57 +0800

    下节课的课程

--- EverDay.py
+++ EverDay.py
@@ -490,6 +490,15 @@ class EverDay:
             return 1
         temp = n%2
         return self.myPow(x,n//2) *self.myPow(x,n//2+temp)
+
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        half = self.myPow(x, n // 2)  # 计算一次子问题
+        if n % 2 == 0:
+            return half * half
+        else:
+            return half * half * x  # 奇数时补乘x
     # End**********************************
 
 # 1429 第一个唯一数字
