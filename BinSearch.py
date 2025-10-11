f27852d46a05383e854a2c0eacb56654e6af077f
Oftongling <liutl1998@qq.com>
2025-10-11 17:15:18 +0800

    完成第三章内容

--- BinSearch.py
+++ BinSearch.py
@@ -30,3 +30,87 @@ def binSearch_hadLeft(nums:list, target):
 # （4）有目标值且重复查找的元素靠右
 
 # 库函数参考bisect https://docs.python.org/zh-cn/3/library/bisect.html
+
+def findPeakElement(nums: list) -> int:
+    l = 0
+    r = len(nums) - 1
+    while l < r:
+        mid = (l + r) // 2
+        if nums[mid - 1] < nums[mid] and nums[mid] > nums[mid + 1]:
+            return nums[mid]
+        elif nums[mid] < nums[mid + 1]:
+            l = mid + 1
+        elif nums[mid] > nums[mid + 1]:
+            r = mid
+    return nums[mid]
+
+def search1(nums: list, target: int) -> int:
+    firstNum = nums[0]
+    l = 0
+    r = len(nums) - 1
+    while l < r:
+        mid = (l + r) // 2 + (l+r) % 2
+        if target >=firstNum: #表示目标值是在第一部分
+            if nums[mid] < firstNum:
+                r = mid - 1
+            else:
+                if nums[mid] > target:
+                    r = mid - 1
+                elif nums[mid] <= target:
+                    l = mid
+        else: # 目标值是在第二部分
+            if nums[mid] > firstNum:
+                l = mid + 1
+            else:
+                if nums[mid] > target:
+                    r = mid - 1
+                elif nums[mid] <= target:
+                    l = mid
+    if l>=len(nums):
+        return -1
+    return l if nums[l] == target else -1
+
+def search(nums: list, target: int) -> int:
+    n = len(nums)
+    if n==0:
+        return -1
+    if n==1:
+        return 0 if nums[0]==target else -1
+    l = 0
+    r = n - 1
+    firstNum = nums[0]
+    while l<=r:
+        mid = (l+r)//2
+        if nums[mid]==target: return mid
+        if firstNum<=nums[mid]:
+            if firstNum <= target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        else:
+            if nums[mid] < target < firstNum:
+                l = mid + 1
+            else:
+                r = mid - 1
+    return -1
+def pow1(number:int,exponent:int)->int:
+    if exponent<0:
+        exponent = -exponent
+        number = 1/number
+    if exponent==1:
+        return number
+    half = pow1(number,exponent//2)
+    return half*half if exponent%2==0 else half*half*number
+
+def pow2(number:int,exponent:int)->int:
+    if exponent<0:
+        exponent = -exponent
+        number = 1/number
+    l = number
+    res = 1
+    while exponent>0:
+        if exponent%2==1:
+            res = l * res
+        exponent = exponent // 2
+        l = l * l
+    return res
--- main.py
+++ main.py
@@ -58,26 +58,14 @@ from Sort import *
 #     i = bisect(breakpoints, score)
 #     print(i)
 #     return grades[i]
-def findPeakElement(nums: list) -> int:
-    l = 0
-    r = len(nums) - 1
-    while l < r:
-        mid = (l + r) // 2
-        if nums[mid - 1] < nums[mid] and nums[mid] > nums[mid + 1]:
-            return nums[mid]
-        elif nums[mid] < nums[mid + 1]:
-            l = mid + 1
-        elif nums[mid] > nums[mid + 1]:
-            r = mid
-    return nums[mid]
 
 
 if __name__ == '__main__':
-    ls = [1,5,7,10,8,6,4,2,1]
-
+    #ls = [4,5,6,7,0,1,2]
+    ls = [1,3]
     # index1 = bisect.bisect(ls, 7)
     # index2 = bisect.bisect_left(ls, 7)
     # index3 = bisect.bisect_right(ls, 7)
     # print("index1 = {}, index2 = {}, index3 = {}".format(index1, index2, index3))
-    res = findPeakElement(ls)
+    res = pow2(5,2)
     print(res)
